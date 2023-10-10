95b599e9d6085f389f968ff5b87dd05a45441c50
MatiCastillo <mcastillo@ms2sgroup.com>
2023-10-10 11:38:49 +0200

    Cambios provider con roles

--- src/main/java/com/ms2sgroup/auth/provider/user/CustomUserStorageProvider.java
+++ src/main/java/com/ms2sgroup/auth/provider/user/CustomUserStorageProvider.java
@@ -15,9 +15,11 @@ import java.util.stream.Stream;
 import org.keycloak.component.ComponentModel;
 import org.keycloak.credential.CredentialInput;
 import org.keycloak.credential.CredentialInputValidator;
+import org.keycloak.models.ClientModel;
 import org.keycloak.models.GroupModel;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.RealmModel;
+import org.keycloak.models.RoleModel;
 import org.keycloak.models.UserModel;
 import org.keycloak.models.credential.PasswordCredentialModel;
 import org.keycloak.storage.StorageId;
@@ -35,6 +37,14 @@ public class CustomUserStorageProvider implements UserStorageProvider,
     private static final Logger log = LoggerFactory.getLogger(CustomUserStorageProvider.class);
     private KeycloakSession ksession;
     private ComponentModel model;
+    
+    private final String role_teacher = "TEACHER";
+    private final String role_professional = "PROFESSIONAL";
+    private final String role_admin = "ADMIN";
+    private final String role_parent="PARENT";
+    private final String role_center_admin="CENTER_ADMIN";
+    
+    private final String client = "client-atenxia";
 
     public CustomUserStorageProvider(KeycloakSession ksession, ComponentModel model) {
         this.ksession = ksession;
@@ -57,7 +67,7 @@ public class CustomUserStorageProvider implements UserStorageProvider,
     public UserModel getUserByUsername(RealmModel realm, String username) {
         log.info("[I41] getUserByUsername({})",username);
         try ( Connection c = DbUtil.getConnection(this.model)) {
-            PreparedStatement st = c.prepareStatement("select username, first_name,last_name, email from atenxia_user where is_active=true and username = ?");
+            PreparedStatement st = c.prepareStatement("select username, '','', email from atenxia_user where is_active=true and username = ?");
             st.setString(1, username);
             st.execute();
             ResultSet rs = st.getResultSet();
@@ -77,7 +87,7 @@ public class CustomUserStorageProvider implements UserStorageProvider,
     public UserModel getUserByEmail(RealmModel realm, String email) {
         log.info("[I48] getUserByEmail({})",email);
         try ( Connection c = DbUtil.getConnection(this.model)) {
-            PreparedStatement st = c.prepareStatement("select username, first_name,last_name, email from atenxia_user where is_active=true and email = ?");
+            PreparedStatement st = c.prepareStatement("select username, '','', email from atenxia_user where is_active=true and email = ?");
             st.setString(1, email);
             st.execute();
             ResultSet rs = st.getResultSet();
@@ -156,7 +166,7 @@ public class CustomUserStorageProvider implements UserStorageProvider,
         log.info("[I113] getUsers: realm={}", realm.getName());
         
         try ( Connection c = DbUtil.getConnection(this.model)) {
-            PreparedStatement st = c.prepareStatement("select username, first_name,last_name, email from atenxia_user where is_active=true and username like ? order by username limit ? offset ?");
+            PreparedStatement st = c.prepareStatement("select username, '','', email from atenxia_user where is_active=true and username like ? order by username limit ? offset ?");
             st.setInt(1, maxResults);
             st.setInt(2, firstResult);
             st.execute();
@@ -177,7 +187,7 @@ public class CustomUserStorageProvider implements UserStorageProvider,
         log.info("[I139] searchForUser: realm={}", realm.getName());
 
         try (Connection c = DbUtil.getConnection(this.model)) {
-            PreparedStatement st = c.prepareStatement("select username, first_name,last_name, email from atenxia_user where is_active=true and username like ? order by username limit ? offset ?");
+            PreparedStatement st = c.prepareStatement("select username, '','', email from atenxia_user where is_active=true and username like ? order by username limit ? offset ?");
             st.setString(1, search);
             st.setInt(2, maxResults);
             st.setInt(3, firstResult);
@@ -213,7 +223,65 @@ public class CustomUserStorageProvider implements UserStorageProvider,
           .lastName(rs.getString("last_name"))
           .build();
         
+        ResultSet roles = null;
+        try ( Connection c = DbUtil.getConnection(this.model)) {
+            PreparedStatement st = c.prepareStatement("select is_center_admin, is_parent, is_professional, is_teacher, is_staff from atenxia_user where is_active = true and username=?");
+            st.setString(1, rs.getString("username"));
+            st.execute();
+            roles = st.getResultSet();
+        } catch (SQLException ex) {
+            throw new RuntimeException("Database error:" + ex.getMessage(), ex);
+        }
+        
+       
+        if(roles!=null)
+        {
+            boolean center_admin = (roles.getBoolean("is_center_admin")) ? true:false;
+            boolean parent = (roles.getBoolean("is_parent")) ? true:false;
+            boolean professional = (roles.getBoolean("is_professional")) ? true:false;
+            boolean teacher = (roles.getBoolean("is_teacher")) ? true:false;
+            boolean admin = (roles.getBoolean("is_staff")) ? true:false;
+            
+            log.info("lookup user by username roles " + center_admin +"," +parent+"," +professional+"," +teacher+"," +admin);
+            
+            user.deleteRoleMapping(getRoleFromString(realm, this.client, this.role_teacher));
+            user.deleteRoleMapping(getRoleFromString(realm, this.client, this.role_parent));
+            user.deleteRoleMapping(getRoleFromString(realm, this.client, this.role_professional));
+            user.deleteRoleMapping(getRoleFromString(realm, this.client, this.role_center_admin));
+            user.deleteRoleMapping(getRoleFromString(realm, this.client, this.role_admin));
+            
+            if (teacher)
+            	user.grantRole(getRoleFromString(realm, this.client, this.role_teacher));
+            if (parent)
+            	user.grantRole(getRoleFromString(realm, this.client, this.role_parent));
+            if (professional)
+            	user.grantRole(getRoleFromString(realm, this.client, this.role_professional));
+            if (center_admin)
+            	user.grantRole(getRoleFromString(realm, this.client, this.role_center_admin));
+            if (admin)
+            	user.grantRole(getRoleFromString(realm, this.client, this.role_admin));
+        }
+      
         return user;
     }
+    
+    
+    private RoleModel getRoleFromString(RealmModel realm, String clientId, String roleName) {
+   	log.info("getRoleFromString " + roleName);
+   	RoleModel role = null;
+           ClientModel client = realm.getClientByClientId(clientId);
+           if (client != null) {
+           	try
+                   {
+           	    role = client.getRole(roleName);
+                   }
+                   catch(Exception e)
+                   {
+               	
+                   }
+           }
+           
+           return role;
+       }
 
 }
