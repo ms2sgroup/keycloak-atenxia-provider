4b64b44fe3219994595f880a3d8b1374ca9a12df
MatiCastillo <mcastillo@ms2sgroup.com>
2023-10-10 10:10:57 +0200

    Nuevo provider

---
+++ src/main/java/com/ms2sgroup/auth/provider/user/CustomUserStorageProviderConstants.java
@@ -0,0 +1,9 @@
+package com.ms2sgroup.auth.provider.user;
+
+public final class CustomUserStorageProviderConstants {
+    public static final String CONFIG_KEY_JDBC_DRIVER = "jdbcDriver";
+    public static final String CONFIG_KEY_JDBC_URL = "jdbcUrl";
+    public static final String CONFIG_KEY_DB_USERNAME = "postgres";
+    public static final String CONFIG_KEY_DB_PASSWORD = "secret";
+    public static final String CONFIG_KEY_VALIDATION_QUERY = "validationQuery";
+}
