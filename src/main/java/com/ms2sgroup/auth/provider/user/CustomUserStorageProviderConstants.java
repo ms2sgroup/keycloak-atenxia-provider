48fc7741d47256acb4ebe0e4261994357f64e625
MatiCastillo <mcastillo@ms2sgroup.com>
2024-01-12 13:01:37 +0100

    Modificacion valor CONFIG_KEY_DB_PASSWORD

--- src/main/java/com/ms2sgroup/auth/provider/user/CustomUserStorageProviderConstants.java
+++ src/main/java/com/ms2sgroup/auth/provider/user/CustomUserStorageProviderConstants.java
@@ -4,6 +4,6 @@ public final class CustomUserStorageProviderConstants {
     public static final String CONFIG_KEY_JDBC_DRIVER = "jdbcDriver";
     public static final String CONFIG_KEY_JDBC_URL = "jdbcUrl";
     public static final String CONFIG_KEY_DB_USERNAME = "postgres";
-    public static final String CONFIG_KEY_DB_PASSWORD = "";
+    public static final String CONFIG_KEY_DB_PASSWORD = "secret";
     public static final String CONFIG_KEY_VALIDATION_QUERY = "validationQuery";
 }
